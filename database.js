34586353047ab7da2ce6b7e1e15360b9936abee4
Matthew-Bowman <matthew.bowman@tutanota.com>
2022-05-31 21:53:55 +0100

    Built constructur with variable initialisation and parameters

--- database.js
+++ database.js
@@ -1,5 +1,9 @@
 module.exports = class Connection {
-    constructor() {
-        
+    constructor(pUser, pPass, pServer, pPort, pDatabase) {
+        this.user = pUser;
+        this.pass = pPass;
+        this.server = pServer;
+        this.port = pPort;
+        this.database = pDatabase;
     }
 }
