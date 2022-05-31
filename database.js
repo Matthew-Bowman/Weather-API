247885067dce2bd8ea6631dff694dc2d7dea7757
Matthew-Bowman <matthew.bowman@tutanota.com>
2022-05-31 22:08:19 +0100

    Restored class & export

--- database.js
+++ database.js
@@ -1,2 +1,18 @@
 const mysql = require(`mysql`);
 
+module.exports.Connection = class Connection {
+    constructor(pUser, pPass, pHost, pPort, pDatabase) {
+        this.user = pUser;
+        this.pass = pPass;
+        this.host = pHost;
+        this.port = pPort;
+        this.database = pDatabase;
+    
+        this.configuration = {
+            host: this.host,
+            user: this.user,
+            password: this.password,
+            database: this.database,
+        }
+    }
+}
