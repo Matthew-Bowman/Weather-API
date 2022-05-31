25eed8cab087f1c8c7a241d8fc5fc824f32a2acb
Matthew-Bowman <matthew.bowman@tutanota.com>
2022-05-31 23:13:10 +0100

    Returned query results

--- database.js
+++ database.js
@@ -45,13 +45,10 @@ module.exports.Connection = class Connection {
         const currentQueryResult = await this.query(currentQuery);
         const dailyQueryResult = await this.query(dailyQuery);
 
-        // Parse Data
-        let currentQueryData = {}
-        let dailyQueryData = []
-
         // Add query results to returnData variable
-        returnData.current = currentQueryData;
-        returnData.daily = dailyQueryData;
+        returnData.current = currentQueryResult;
+        returnData.daily = dailyQueryResult;
+
         // Return
         return returnData;
     }
