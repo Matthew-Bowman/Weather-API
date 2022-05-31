f0e839e27936a8c795b4680c2d8fe4cab011b3bc
Matthew-Bowman <matthew.bowman@tutanota.com>
2022-05-31 21:50:00 +0100

    Required database.js

--- index.js
+++ index.js
@@ -1,9 +1,11 @@
 const app = require(`express`)();
 const port = 8080;
 
+const database = require(`./database`);
+
 app.get('/london', (req, res) => {
     res.status(200).send({
-        
+
     });
 });
 
