779a1ba430eb52b36e9853e31336ec14bd62d10d
Matthew-Bowman <matthew.bowman@tutanota.com>
2022-05-31 22:02:53 +0100

    Removed export & class

--- database.js
+++ database.js
@@ -1,18 +1,2 @@
 const mysql = require(`mysql`);
 
-module.exports = class Connection {
-    constructor(pUser, pPass, pHost, pPort, pDatabase) {
-        this.user = pUser;
-        this.pass = pPass;
-        this.host = pHost;
-        this.port = pPort;
-        this.database = pDatabase;
-    
-        this.configuration = {
-            host: this.host,
-            user: this.user,
-            password: this.password,
-            database: this.database,
-        }
-    }
-}
