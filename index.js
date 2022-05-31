f44b7db1f2171dd8af8e682b8badeb3ed7a98186
Matthew-Bowman <matthew.bowman@tutanota.com>
2022-05-31 22:24:23 +0100

    Implemented use of env variables for database connection

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const port = 8080;
 require(`dotenv`).config();
 
 const database = require(`./database`);
-const connection = new database.Connection(`root`, ``, `localhost`, 3306, `weather_api`);
+const connection = new database.Connection(process.env.DB_USERNAME, process.env.DB_PASSWORD, process.env.DB_HOST, process.env.DB_PORT, process.env.DB_DATABASE);
 
 app.get('/london', (req, res) => {
     res.status(200).send({
