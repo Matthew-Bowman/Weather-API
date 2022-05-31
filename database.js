bbe0966cb6d227adf2ab2eaa4daea2f0f4041d6a
Matthew-Bowman <matthew.bowman@tutanota.com>
2022-05-31 22:09:30 +0100

    Added mysql connection to constructur

--- database.js
+++ database.js
@@ -2,17 +2,24 @@ const mysql = require(`mysql`);
 
 module.exports.Connection = class Connection {
     constructor(pUser, pPass, pHost, pPort, pDatabase) {
+        // Class Variables
         this.user = pUser;
         this.pass = pPass;
         this.host = pHost;
         this.port = pPort;
         this.database = pDatabase;
     
+        // Configuration Object
         this.configuration = {
             host: this.host,
+            port: this.port,
             user: this.user,
             password: this.password,
             database: this.database,
         }
+
+        // Database Connection Initialisation
+        this.connection = mysql.createConnection(configuration);
+        this.connection.connect();
     }
 }
