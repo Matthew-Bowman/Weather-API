a0321094ea1ab026811bdf7456bc0144cd8322ea
Matthew-Bowman <matthew.bowman@tutanota.com>
2022-05-31 22:30:32 +0100

    Imported updater.js

--- index.js
+++ index.js
@@ -6,6 +6,8 @@ require(`dotenv`).config();
 const database = require(`./database`);
 const connection = new database.Connection(process.env.DB_USERNAME, process.env.DB_PASSWORD, process.env.DB_HOST, process.env.DB_PORT, process.env.DB_DATABASE);
 
+const updater = require(`./updater`);
+
 app.get('/london', (req, res) => {
     res.status(200).send(connection.GetData());
 });
