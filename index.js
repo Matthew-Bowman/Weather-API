8fce7f9e9aa77e81298d9a1f3a427d6a68620de4
Matthew-Bowman <matthew.bowman@tutanota.com>
2022-05-31 22:14:45 +0100

    Created database connection

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ const app = require(`express`)();
 const port = 8080;
 
 const database = require(`./database`);
+const connection = new database.Connection(`root`, ``, `localhost`, 3306, `weather_api`);
 
 app.get('/london', (req, res) => {
     res.status(200).send({
