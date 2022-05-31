dedfaf592136924eeaa976f0ab2253271ce04ee9
Matthew-Bowman <matthew.bowman@tutanota.com>
2022-05-31 20:46:09 +0100

    Added empty send data to london endpoint

--- index.js
+++ index.js
@@ -2,7 +2,9 @@ const app = require(`express`)();
 const port = 8080;
 
 app.get('/london', (req, res) => {
-    res.status(200);
+    res.status(200).send({
+        
+    });
 });
 
 app.listen(port, () => {
