861047b1ee554eff06360517266caf192a72ede4
Matthew-Bowman <matthew.bowman@tutanota.com>
2022-06-01 04:21:51 +0100

    Turned GetData function async

--- index.js
+++ index.js
@@ -8,58 +8,61 @@ require(`dotenv`).config();
 const updater = require(`./updater`);
 
 app.get('/london', (req, res) => {
-    // connection.GetData().then(data => {
-    //     res.status(200).send(data);
-    // })
+    GetData().then(data => {
+        res.status(200).send(data);
+    })
 });
 
 app.listen(process.env.PORT, () => {
     console.log(`Live on: http://localhost:${process.env.PORT}`);
 });
 
-setInterval(updater.Update, 60000, connection);
+// setInterval(updater.Update, 60000, connection);
 
-function GetData() {
-    etch(`https://api.openweathermap.org/data/2.5/onecall?lat=51.5072&lon=0.1276&exclude=minutely,hourly&appid=${process.env.WEATHER_KEY}`)
-            .then(res => res.json())
-            .then(data => {
+async function GetData() {
+    returnData = {
+        current: {
 
-                returnData = {
-                    current: {
+        },
+        daily: [
 
-                    },
-                    daily: [
+        ]
+    };
+    
+    await fetch(`https://api.openweathermap.org/data/2.5/onecall?lat=51.5072&lon=0.1276&exclude=minutely,hourly&appid=${process.env.WEATHER_KEY}`)
+        .then(res => res.json())
+        .then(data => {
 
-                    ]
-                }
 
+
+            // Prepare Data
+            const currentData = {
+                location: "London",
+                name: data.current.weather[0].main,
+                icon: data.current.weather[0].icon,
+                temperature: data.current.temp,
+                min_temperature: data.daily[0].temp.min,
+                max_temperature: data.daily[0].temp.max,
+                wind_speed: data.current.wind_speed,
+                rain_probability: data.daily[0].pop,
+                precipitation: data.daily[0].rain,
+            }
+            returnData.current = currentData;
+
+            for (entry of data.daily) {
                 // Prepare Data
-                const currentData = {
-                    location: "London",
-                    name: data.current.weather[0].main,
-                    icon: data.current.weather[0].icon,
-                    temperature: data.current.temp,
-                    min_temperature: data.daily[0].temp.min,
-                    max_temperature: data.daily[0].temp.max,
-                    wind_speed: data.current.wind_speed,
-                    rain_probability: data.daily[0].pop,
-                    precipitation: data.daily[0].rain,
-                }
-                returnData.current = currentData;
-
-                for(let i = 0; i < 7; i++) {
-                    // Prepare Data
-                    const dailyData = {
-                        datetime: data.daily[i].dt,
-                        icon: data.daily[i].weather[0].icon,
-                        min_temperature: data.daily[i].temp.min,
-                        max_temperature: data.daily[i].temp.max,
-                    }
-
-                    returnData.daily.push(dailyData);
+                const dailyData = {
+                    datetime: entry.dt,
+                    icon: entry.weather[0].icon,
+                    min_temperature: entry.temp.min,
+                    max_temperature: entry.temp.max,
                 }
 
+                returnData.daily.push(dailyData);
+            }
 
-                return returnData;
-            });
+
+        });
+
+    return returnData;
 }
